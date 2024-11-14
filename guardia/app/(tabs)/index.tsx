2709e7b3065bf66943d581faa49aa040f2fc5a58
jacobkavanal <jake.kavanal@gmail.com>
2024-11-13 21:45:15 -0800

    added map functionality

--- guardia/app/(tabs)/index.tsx
+++ guardia/app/(tabs)/index.tsx
@@ -3,12 +3,42 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
+import MapView, {Geojson} from 'react-native-maps';
+
+import { useEffect, useState } from 'react';
+
+const initial_loc = {
+  latitude: 32.7157,
+  longitude: -117.1611,
+  latitudeDelta: .5,
+  longitudeDelta: .5,
+};
+
+const myPlace = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [64.165329, 48.844287],
+      }
+    }
+  ]
+};
+
 export default function TabOneScreen() {
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <MapView 
+      style={styles.map}
+      region={initial_loc}
+      showsUserLocation
+      showsMyLocationButton
+      >
+      </MapView>
     </View>
   );
 }
@@ -28,4 +58,8 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  map: {
+    width: '100%',
+    height: '100%',
+  }
 });
